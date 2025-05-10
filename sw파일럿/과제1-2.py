0cc60f2ed63ee51857faaa0cc40e34ef07099606
wellseekogi <es2006tiger@gmail.com>
2025-05-10 12:56:46 +0900

    변경

--- sw파일럿/과제1-2.py
+++ sw파일럿/과제1-2.py
@@ -1,11 +1,17 @@
-f = open("C:/Users/User/Desktop/timestamp,event,message.txt", mode = 'r')
-data = f.read()
-li = data.split(',')
+with open("C:/Users/User/Desktop/timestamp,event,message.txt", mode='r') as f:
+    lines = f.readlines()
+di = {}
+for i in range(35,0,-1):
+    li = lines[i].split(',')
+    di[li[0]] = li[2]
 
-print(li)
+
+fi = open('C:/Users/User/swpilot/sw파일럿/mission_computer_main.json', mode = 'w')
+fi.write(str(di))
+
+
 f.close()
-dateli = []
-storyli = []
+
 
 
 
